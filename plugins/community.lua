bebe10c524b67c40990ecfd630310a7a08642c40
IceSword511 <icesword511@gmail.com>
2024-02-20 20:16:52 +0800

    Update: 1. Customize community 2. Update colorscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,15 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin", enable = true },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim", enable = false },
+  { import = "astrocommunity.colorscheme." },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.split-and-window.edgy-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
